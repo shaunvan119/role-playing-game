ecc6d25f7e77918d40fb7d12f4792db6741214ed
shaunvan119 <shaunvan@live.com.au>
2022-04-14 08:47:21 +1000

    data varibles

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+const heroElementId = "hero"
+const heroName = "Wizard"
+const heroAvatar = "images/wizard.png"
+const heroHealth = "60"
+const heroDiceRoll = 6
+
+const monsterElementId = "monster"
+const monsterName = "Orc"
+const monsterAvatar = "images/orc.png"
+const monsterHealth = "10"
+const monsterDiceRoll = 4
+
+
+
+
+
+
+
 document.getElementById("hero").innerHTML = `
-<div class="character-card">
+    <div class="character-card">
         <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
+        <img class="avatar" src="assets/wizard.png">
         <p class="health">health: <b> 60 </b></p>
         <div class="dice-container"><div class="dice"> 6 </div></div>
     </div> 
 `
 
 document.getElementById("monster").innerHTML = `
-                <div class="character-card">
-                    <h4 class="name"> Orc </h4>
-                    <img class="avatar" src="assets/orc.png"/>
-                    <p class="health">health: <b> 10 </b></p>
-                    <div class="dice-container"><div class="dice"> 4 </div></div>
-                </div>
+    <div class="character-card">
+        <h4 class="name"> Orc </h4>
+        <img class="avatar" src="assets/orc.png"/>
+        <p class="health">health: <b> 10 </b></p>
+        <div class="dice-container"><div class="dice"> 4 </div></div>
+    </div>
 `
