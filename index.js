04f697ce969b977b8093d349b7f98c84a7e13ad9
shaunvan119 <shaunvan@live.com.au>
2022-04-14 12:40:52 +1000

    refactored render function

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const heroElementId = "hero"
 const heroName = "Wizard"
 const heroAvatar = "assets/wizard.png"
-const heroHealth = "60"
+const heroHealth = 60
 const heroDiceRoll = 6
 
 const monsterElementId = "monster"
 const monsterName = "Orc"
 const monsterAvatar = "assets/orc.png"
-const monsterHealth = "10"
+const monsterHealth = 10
 const monsterDiceRoll = 4
 
-
-
-function renderCharacter(elementId, name, avatar, health, diceRoll){
-  document.getElementById(elementId).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health}</b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
-    </div>`;   
+function renderCharacter(elementId, name, avatar, health, diceRoll) {
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">
+                <div class="dice"> ${diceRoll} </div>
+            </div>
+        </div>`
 }
 
-renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll)
-renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll)
+renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll);
+renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll);
