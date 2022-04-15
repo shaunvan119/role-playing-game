d62c52000ae9e4c450232d693e0ea21c7647913c
shaunvan119 <shaunvan@live.com.au>
2022-04-15 17:06:54 +1000

    constructor function set up

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const hero = {
     name: "Wizard",
     avatar: "assets/wizard.png",
     health: 60,
-    diceRoll: [3, 1, 4], 
     diceCount: 3
 }
 
@@ -25,8 +24,17 @@ const monster = {
     name: "Orc",
     avatar: "assets/orc.png",
     health: 10,
-    diceRoll: [6],
     diceCount: 1
+}
+
+
+//constructor function
+function Character(data) {
+    this.elementId = data.elementId 
+    this.name = data.name
+    this.avatar = data.avatar
+    this.health = data.health
+    this.diceCount = data.diceCount
 }
 
 function renderCharacter(data) {
