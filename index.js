1885a1cb51e52006ca1eb57f036b3efadaf227a8
shaunvan119 <shaunvan@live.com.au>
2022-04-16 08:22:40 +1000

    added object.assign to character function

--- index.js
+++ index.js
@@ -30,12 +30,8 @@ const monster = {
 
 //constructor function
 function Character(data) {
-    this.elementId = data.elementId;
-    this.name = data.name;
-    this.avatar = data.avatar;
-    this.health = data.health;
-    this.diceCount = data.diceCount;
-
+    
+    Object.assign(this, data)
     this.getCharacterHtml = function () {
         const { elementId, name, avatar, health, diceCount } = this;
         const diceHtml = getDiceHtml(diceCount)
