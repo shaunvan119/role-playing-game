f89ac1bb0e5a70b35107f162a206adf9c983e28f
shaunvan119 <shaunvan@live.com.au>
2022-04-16 10:27:10 +1000

    added character.js file

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 import charData from "./data.js";
-import getDiceRollArray from "./utils.js"
+
+import Character from "./Character.js";
 
 
 
 
-function Character(data) {
-    Object.assign(this, data)
-    
-     this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
-        }).join('')
-    }
 
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
-        let diceHtml = this.getDiceHtml(diceCount);
-        
-           return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}" />
-                <div class="health">health: <b> ${health} </b></div>
-                <div class="dice-container">
-                    ${diceHtml}
-                </div>
-            </div>`;
-    }  
-}
 
 
 const wizard = new Character(charData.hero)
