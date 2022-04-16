bfa3518ff0e2a1a761df503a513fb50c6f8da7a1
shaunvan119 <shaunvan@live.com.au>
2022-04-16 10:01:29 +1000

    character data varible to hold all data

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ function getDiceRollArray(diceCount) {
     });   
 }
 
-const hero = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "assets/wizard.png",
-    health: 60,
-    diceCount: 3
+const characterData = {
+        hero:{
+            elementId: "hero",
+            name: "Wizard",
+            avatar: "assets/wizard.png",
+            health: 60,
+            diceCount: 3
+        },
+
+        monster: {
+            elementId: "monster",
+            name: "Orc",
+            avatar: "assets/orc.png",
+            health: 10,
+            diceCount: 1
+        }
 }
 
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "assets/orc.png",
-    health: 10,
-    diceCount: 1
-}
+
+
+
 
 function Character(data) {
     Object.assign(this, data)
@@ -46,8 +52,8 @@ function Character(data) {
 }
 
 
-const wizard = new Character(hero)
-const orc = new Character(monster)
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 
 function render() {
     document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
