32ace33e01cee720d2ca1b16bf9bc1d935fd0783
shaunvan119 <shaunvan@live.com.au>
2022-04-17 16:03:50 +1000

    connected attack function

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import charData from "./data.js";
 import Character from "./Character.js";
 
+function attack() {
+    render()
+}
 
-const wizard = new Character(charData.hero)
-const orc = new Character(charData.monster)
 
 function render() {
     document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
     document.getElementById("monster").innerHTML = orc.getCharacterHtml()
 }
+
+document.getElementById("attack-button").addEventListener("click", attack)
+const wizard = new Character(charData.hero)
+const orc = new Character(charData.monster)
 
 render()
