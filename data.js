d32d1441df7ff8b081a30d2306dcecd6ba75a2af
shaunvan119 <shaunvan@live.com.au>
2022-04-19 19:58:27 +1000

    added adtional data hero, orc, demon, goblin

--- data.js
+++ data.js
@@ -1,18 +1,30 @@
 const characterData = {
     hero: {
-        
         name: "Wizard",
-        avatar: "assets/wizard.png",
-        health: 10,
+        avatar: "images/wizard.png",
+        health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
-    monster: {
-        
+    orc: {
         name: "Orc",
-        avatar: "assets/orc.png",
-        health: 100,
+        avatar: "images/orc.png",
+        health: 30,
         diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.png",
+        health: 20,
+        diceCount: 3,
         currentDiceScore: []
     }
 }
