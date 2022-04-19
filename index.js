0d1f078cfa97e63a97e3770e997f0c7b78bf34d3
shaunvan119 <shaunvan@live.com.au>
2022-04-19 11:53:09 +1000

    teneray operator with document body div

--- index.js
+++ index.js
@@ -16,11 +16,16 @@ function attack() {
 
 function endGame(){
     const endMessage = wizard.health === 0 && orc.health === 0 ?
-    "No victors - all creatures are dead" :
-    wizard.health > 0 ? "The Wizard Wins" :
-    "The Orc is Victorious"
-    
-    console.log(endMessage)
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+        "The Orc is Victorious"
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = 
+        `<div class="end-game">
+            <h2>Game Over</h2>
+            <h3>${endMessage}</h3>
+            <p class="end-emoji">${endEmoji}</p>
+        </div>` 
 }
 
 
